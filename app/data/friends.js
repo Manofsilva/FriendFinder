cceddb16702ab15711c973815f15f3e38e5ec43d
Cesar Silva <silvacde@gmail.com>
2019-09-19 13:30:13 -0700

    added test photo link

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var userData = [
     {
       "name": "Ahmed",
-      "photo": "https://www.shutterstock.com/image-photo/young-man-crossed-arms-over-gray-368732933?src=CHE4TKMW8SbTFZdIRCmWKQ-1-55",
+      "photo": "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/25659646_10155885815970690_303656648272584439_n.jpg?_nc_cat=107&_nc_oc=AQm8hV4Z4lCuj8z_HOS5jUNE9qT9rVTUWVMEA57hKNZPOsPqUueFfOLDEtu0kpH76KA&_nc_ht=scontent-sea1-1.xx&oh=51de36676bf5e3365e82b01ce23fa6ca&oe=5E35E322",
       "scores":[
           5,
             1,
