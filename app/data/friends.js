e743f78f98a0e367c75d4ec81589847c79319978
Cesar Silva <silvacde@gmail.com>
2019-09-21 16:56:14 -0700

    added pictures to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@
 var userData = [
     {
       "name": "Ahmed",
-      "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdEUTYt_BzMCpis4FtXlzMwGBdD1RDP3z0MOfanNhnX8nhiLMFYA",
       "scores":[
           5,
             1,
@@ -19,7 +19,7 @@ var userData = [
     },
     {
       "name": "Joe",
-      "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVW10LT2nunc683pTEx2g4eFkucRgkc9mXDd4VvXyBsFAdSYqzfw",
       "scores":[
           3,
           2,
@@ -33,22 +33,87 @@ var userData = [
           1
       ]
     },
+    
     {
-      "name": "Jane",
-      "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
-      "scores":[
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1
-      ]
-    }
+        "name": "Michelle",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy0AZWYNtDgOMrRYDiQDtai5wOrRCvrOdlCpuCQ57vvIeqSWY3",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+      }, 
+      {
+        "name": "Sara",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRERRpbCqCcjZOP4psHDzlYgC-wc3d8I756grP2oqVknYePP6fc",
+        "scores":[
+            5,
+            5,
+            5,
+            4,
+            4,
+            4,
+            3,
+            3,
+            3,
+            1
+        ]
+      }, 
+      {
+        "name": "Chelsea",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAzNSVO7hJM1Vs9tjlSKWkiAeXhamnrAu4wimZLig-lrAUCQpY",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+      }, 
+      {
+        "name": "Fred",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrn7Dz4axjTTbABjssndlaDkYW6Hrw-K4FlnFqPHIYqrYYiDGqRg",
+        "scores":[
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+      }, 
+      {
+        "name": "Luke",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3AvK2tKvY3xjrPpalW7Alg4RIIEWNktt0s9Jq5sQTvt3M4FRa8w",
+        "scores":[
+            4,
+            1,
+            5,
+            1,
+            3,
+            1,
+            2,
+            1,
+            5,
+            1
+        ]
+      }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -12,8 +12,7 @@ var userData = require("../app/data/friends");
 module.exports = function(app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // In each of the below cases when a user visits a link they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
   
     app.get("/api/friends", function(req, res) {
