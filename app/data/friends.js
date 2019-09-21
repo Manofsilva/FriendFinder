8d3f4400229da10bc15828b6fb6f0317d3108fa7
Cesar Silva <silvacde@gmail.com>
2019-09-20 22:59:29 -0700

    added more dummy friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,9 @@
+
+
 var userData = [
     {
       "name": "Ahmed",
-      "photo": "",
+      "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
       "scores":[
           5,
             1,
@@ -14,7 +16,40 @@ var userData = [
             4,
             1
       ]
+    },
+    {
+      "name": "Joe",
+      "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+      "scores":[
+          3,
+          2,
+          2,
+          4,
+          1,
+          1,
+          5,
+          1,
+          3,
+          1
+      ]
+    },
+    {
+      "name": "Jane",
+      "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+      "scores":[
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
+      ]
     }
-]
-// Note how we export the array. This makes it accessible to other files using require.
-module.exports = userData;
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = userData;
